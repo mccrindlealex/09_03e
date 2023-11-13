08283d5f6cfd0a4e521c09c671e11e0025376758
Alex McCrindle <mccrindlealex@gmail.com>
2023-11-13 11:44:42 -0500

    Linkedin Git Repo

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const showExpectedResult = false
+const showHints = false
+
+
+const forms = document.querySelectorAll("form");
+
+forms.forEach((form)=>{
+form.addEventListener("submit", (event))
+} )
+
+
+
+
+
+function updatePage(button, argument) {
+    // Your code goes here.
+}
